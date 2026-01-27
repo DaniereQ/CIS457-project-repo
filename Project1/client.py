d4a47424bfe2438a94d9d519c53a997b4b19482d
Quentin Daniere <quentindaniere@gmail.com>
2026-01-26 21:17:10 -0500

    basic implementation of sending a message with client and receiving with server

--- Project1/client.py
+++ Project1/client.py
@@ -1,1 +1,30 @@
-# Initial File
+"""
+Project 1 Client Python File
+
+Names: Quentin Daniere, Luke Erlewein, Lucas
+"""
+
+import socket
+
+class Client:
+    def __init__(self):
+        self.host = socket.gethostname() # this is getting ur own address to send a message to yourself
+        self.port = 5800
+
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def send_message(self, msg: str):
+        try:
+            self.client_socket.connect(self.host, self.port)
+        except:
+            print("The connection failed")
+            return
+        
+        print(f'Sending: {msg}')
+        self.client_socket.sendall(msg.encode())
+        self.client_socket.close()
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.send_message("Hello World!")
--- Project1/server.py
+++ Project1/server.py
@@ -1,1 +1,33 @@
-# Initial Commit
+"""
+Project 1 Client Python File
+
+Names: Quentin Daniere, Luke Erlewein, Lucas
+"""
+
+import socket
+
+class Server:
+    def __init__(self):
+        self.host = socket.gethostname()
+        self.port = 5800
+
+        self.listen_for_messages()
+    
+    def listen_for_messages(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.bind((self.host, self.port))
+
+            # listen for messages
+            server.listen()
+            
+            connection, addresss = server.accept()
+            with connection:
+                while True:
+                    data = connection.recv(2048)
+                    if not data:
+                        break
+                
+                print(f'Received: {data.decode()}')
+
+if __name__ == "__main__":
+    server = Server()
